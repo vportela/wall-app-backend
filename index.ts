92de1b458eed3522178b2607a18795a71086723a
vportela <victoria.portela03@gmail.com>
2022-05-28 15:43:54 -0400

    called api

--- index.ts
+++ index.ts
@@ -1,19 +1,38 @@
 import express, { Request , Response } from "express";
-
+import cors from "cors";
 
 const app = express()
 
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 
 app.get("/", (request: Request, response: Response) => { 
     console.log("console.log, hello from the home directory")
-    response.send("hello from the home directory")
+    const user = { 
+        firstName: "tom",
+        lastName: "grdkfh",
+        email: "tom@gmail.com"
+    }
+    response.send(user)
 })
 
 app.get("/posts", (request: Request, response: Response) => { 
-    response.send("these are posts hello")
+    console.log("fetching posts")
+    const newMessageArray = [
+        { 
+        id: 1,
+        user: "newUser",
+        text: "this is text"
+        }
+    ]
+    response.send(newMessageArray)
+   
 })
+
+
 
 //starting a server on port 5000
 app.listen(5000, () => { 
-    console.log(`testing`)
+    console.log(`succesfully running backend on port 5000!!`)
 })
