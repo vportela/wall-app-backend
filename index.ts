60d8e205e4fd759a6d42409a4d18c08d13434597
vportela <victoria.portela03@gmail.com>
2022-05-28 14:01:09 -0400

    added script

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
-import express from "express";
+import express, { Request , Response } from "express";
+
 
 const app = express()
 
-app.get("/victoriaLovesCookies", (request, response) => { 
+
+app.get("/", (request: Request, response: Response) => { 
     console.log("console.log, hello from the home directory")
     response.send("hello from the home directory")
 })
 
+app.get("/posts", (request: Request, response: Response) => { 
+    response.send("these are posts hello")
+})
+
 //starting a server on port 5000
 app.listen(5000, () => { 
-    console.log(`yay my server is running on port`)
+    console.log(`testing`)
 })
